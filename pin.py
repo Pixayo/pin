32f208ffa976ab17081badeae3e49e1705584e2f
Pixayo <kaio.rodrigo729@gmail.com>
2026-02-07 17:56:15 -0300

    Add exclusive group and try-exept structure

--- pin.py
+++ pin.py
@@ -12,45 +12,45 @@ def main():
         description='Basic shell snippet manager'
     )
 
-    parser.add_argument('snippet', nargs='?', help='Snippet name to use')
+    # TODO: extra argument for "--list", searching snippets by reference
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-a', '--add', type=str, metavar='COMMAND', help='Add new snippet')
+    group.add_argument('-c', '--change', type=str, metavar='COMMAND', help='change snippet command')
+    group.add_argument('-r', '--remove', action='store_true', help='remove snippet')
+    group.add_argument('-l', '--list', action='store_true', help='List all snippets')
 
-    parser.add_argument('-a', '--add', type=str, metavar='COMMAND', help='Add new snippet')
-    parser.add_argument('-c', '--change', type=str, metavar='COMMAND', help='change snippet command')
-    parser.add_argument('-r', '--remove', action='store_true', help='remove snippet')
-    # TODO: extra argument for searching snippets
-    parser.add_argument('-l', '--list', action='store_true', help='List all snippets')
-
-    parser.add_argument('--create-config', action='store_true', help='create default config')
+    group.add_argument('--create-config', action='store_true', help='create default config')
     # TODO: Implement appending different config files
-    # parser.add_argument('--append-config', action='store_true', help='Append snippets from home config and current directory config file')
+    # group.add_argument('--append-config', action='store_true', help='Append snippets from home config and current directory config file')
+
+    # Main argument
+    parser.add_argument('snippet', nargs='?', help='Snippet name to use')
 
     args = parser.parse_args()
 
-    if args.create_config:
-        create_config(CONFIG_PATH)
-        return
-
-    config = load_config(CONFIG_PATH)
-
-    # Handling flags
-    # FIXME: Every flag should be used alone, if more that one is specified, raise an error
-    if args.add or args.remove or args.change:
-        modify_snippets(config, args)
-    elif args.list:
-        list_snippets(config)
-    
-    # Primary action
     # TODO: use shell expressions to evaluate and execute
-    # FIXME: modifications in the error handling are not handle properly
-    snippet = args.snippet
+    try:
+        if args.create_config:
+            create_config(CONFIG_PATH)
+            return
 
-    if snippet:
-        if snippet in config:
-            print(config[snippet])
+        config = load_config(CONFIG_PATH)
+
+        if args.add or args.remove or args.change:
+            modify_snippets(config, args)
+        elif args.list:
+            list_snippets(config)
+        elif args.snippet:
+            if args.snippet in config:
+                print(config[args.snippet])
+            else:
+                print(f'snippet "{args.snippet}" not found')
         else:
-            print(f'snippet "{snippet}" not found')
-    else:
-        parser.print_help()
+            parser.print_help()
+    except (ValueError, FileNotFoundError) as e:
+        print(e)
+        sys.exit(1)
+
 
 # --- Usages ---
 
@@ -110,9 +110,6 @@ def create_config(path: Path):
 
 
 def save_config(path: Path, config: dict):
-    if not path.exists():
-        raise FileNotFoundError(f'config file not found in {path.absolute()}')
-        
     try:
         with open(path, 'w', encoding='utf-8') as file:
             json.dump(config, file, indent=2, ensure_ascii=False)
