09aeecc8bd0d578f845aefca75285b910e1c4be5
Pixayo <kaio.rodrigo729@gmail.com>
2026-02-07 16:04:41 -0300

    Small changes

--- pin.py
+++ pin.py
@@ -17,7 +17,7 @@ def main():
     parser.add_argument('-a', '--add', type=str, metavar='COMMAND', help='Add new snippet')
     parser.add_argument('-c', '--change', type=str, metavar='COMMAND', help='change snippet command')
     parser.add_argument('-r', '--remove', action='store_true', help='remove snippet')
-    # TODO: add search snippets from list with grep
+    # TODO: extra argument for searching snippets
     parser.add_argument('-l', '--list', action='store_true', help='List all snippets')
 
     parser.add_argument('--generate-config', action='store_true', help='create default config')
@@ -34,11 +34,12 @@ def main():
 
     # Handling flags
     if args.add or args.remove or args.change:
-        modify_config(config, args)
+        modify_snippets(config, args)
     elif args.list:
         list_snippets(config)
     
     # Primary action
+    # TODO: use shell expressions to evaluate and execute
     snippet = args.snippet
 
     if snippet:
@@ -49,9 +50,9 @@ def main():
     else:
         parser.print_help()
 
-# --- Usages ---
+# --- Usages --- exit code 11~19
 
-def modify_config(config, args):
+def modify_snippets(config, args):
     snippet = args.snippet
 
     if not snippet:
@@ -73,45 +74,57 @@ def modify_config(config, args):
     elif args.remove:
         config.pop(snippet, None)
 
-    with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
-        json.dump(config, file, indent=2, ensure_ascii=True)
-
+    save_config(config)
     sys.exit(0)
 
 
 def list_snippets(config):
+    # TODO: Implement searching logic if a extra argument is passed
+    
     print(f"{'SNIPPET':<15} | {'COMMAND'}")
     for name, command in config.items():
         print(f"{name:<15} | {command}")
     
     sys.exit(0)
 
-# --- JSON manipulation ---
+# --- JSON manipulation --- exit code 2~9
 
 def load_config():
     try:
-        with open(CONFIG_PATH, 'r', encoding='utf-8') as f:
-            return json.load(f)
+        with open(CONFIG_PATH, 'r', encoding='utf-8') as file:
+            return json.load(file)
     except FileNotFoundError:
         print(f'Erro: config file not found in {CONFIG_PATH.absolute()}')
-        sys.exit(3)
+        sys.exit(2)
     except json.JSONDecodeError:
         print(f'Erro: invalid json config file {CONFIG_PATH.absolute()}.')
-        sys.exit(3)
+        sys.exit(2)
 
 
 def create_config():
     if CONFIG_PATH.exists():
         print(f'Erro: config file already exists in {CONFIG_PATH.absolute()}')
-        sys.exit(2)
+        sys.exit(3)
     
     default_config = {}
     default_config['hello'] = 'echo "Hello World!"'
 
     with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
-        json.dump(default_config, file, indent=2, ensure_ascii=True)
+        json.dump(default_config, file, indent=2, ensure_ascii=False)
 
     print(f'Config file created in {CONFIG_PATH.absolute()}')
+
+
+def save_config(config: dict):
+    try:
+        with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+            json.dump(config, file, indent=2, ensure_ascii=False)
+    except FileNotFoundError:
+        print(f'Erro: config file not found in {CONFIG_PATH.absolute()}')
+        sys.exit(4)
+    except json.JSONDecodeError:
+        print(f'Erro: invalid json config file {CONFIG_PATH.absolute()}.')
+        sys.exit(4)
 
 
 if __name__ == '__main__':
