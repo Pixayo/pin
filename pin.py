93b032cb8c2f25d548bd7e06e25f36de83a68beb
Pixayo <kaio.rodrigo729@gmail.com>
2026-02-07 15:05:43 -0300

    Implement basic use cases

--- pin.py
+++ pin.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser
 from pathlib import Path
 
 
@@ -16,11 +16,12 @@ def main():
     parser.add_argument('snippet', nargs='?', help='Snippet name to use')
 
     parser.add_argument('-a', '--add', type=str, metavar='COMMAND', help='Add new snippet')
+    parser.add_argument('-c', '--change', type=str, metavar='COMMAND', help='change snippet command')
+    parser.add_argument('-r', '--remove', action='store_true', help='remove snippet')
     parser.add_argument('-l', '--list', action='store_true', help='List all snippets')
-    # parser.add_argument('-r', help='remove snippet or category', action='store_true')
-    # parser.add_argument('-c', help='change snippet expression')
 
     parser.add_argument('--generate-config', action='store_true', help='create default config')
+    parser.add_argument('--current-dir', action='store_true', help='look for config file in the current directory')
 
     args = parser.parse_args()
 
@@ -31,32 +32,43 @@ def main():
     config: dict = load_config()
 
     # Handling flags
-    if args.add:
-        add_snippet(config, args.add, args.snippet)
+    if args.add or args.remove or args.change:
+        modify_config(config, args)
     elif args.list:
         list_snippets(config)
     
-    if args.snippet:
-        if args.snippet in config:
-            print(config[args.snippet])
+    # Primary action
+    if snippet:
+        if snippet in config:
+            print(config[snippet])
         else:
-            print(f'Erro: snippet {snippet} not found')
+            print(f'Erro: snippet "{snippet}" not found')
     else:
         parser.print_help()
 
 # --- Usages ---
 
-def add_snippet(config: dict, command: str, snippet: str):
+def modify_config(config, args):
+    snippet = args.snippet
+
     if not snippet:
         print('Erro: snippet not specified')
-        print('Usage: pin -a [COMMAND] [SNIPPET]')
         sys.exit(11)
     
-    if snippet in config:
-        print(f'Erro: snippet {snippet} already exists')
-        sys.exit(11)
-    
-    config[snippet] = command
+    if args.add:
+        if snippet in config:
+            print(f'Erro: snippet "{snippet}" already exists')
+            sys.exit(11)
+
+        config[snippet] = args.add
+    elif args.change:
+        if snippet not in config:
+            print(f'Erro: snippet "{snippet}" not found')
+            sys.exit(11)
+
+        config[snippet] = args.change
+    elif args.remove:
+        config.pop(snippet, None)
 
     with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
         json.dump(config, file, indent=2, ensure_ascii=True)
@@ -66,13 +78,24 @@ def add_snippet(config: dict, command: str, snippet: str):
 
 def list_snippets(config):
     print(f"{'SNIPPET':<15} | {'COMMAND'}")
-    print("-" * 30)
     for name, command in config.items():
         print(f"{name:<15} | {command}")
     
     sys.exit(0)
 
 # --- JSON manipulation ---
+
+def load_config():
+    try:
+        with open(CONFIG_PATH, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f'Erro: config file not found in {CONFIG_PATH.absolute()}')
+        sys.exit(3)
+    except json.JSONDecodeError:
+        print(f'Erro: invalid json config file {CONFIG_PATH.absolute()}.')
+        sys.exit(3)
+
 
 def create_config():
     if CONFIG_PATH.exists():
@@ -86,18 +109,6 @@ def create_config():
         json.dump(default_config, file, indent=2, ensure_ascii=True)
 
     print(f'Config file created in {CONFIG_PATH.absolute()}')
-
-
-def load_config():
-    try:
-        with open(CONFIG_PATH, 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        print(f'Erro: config file not found in {CONFIG_PATH.absolute()}')
-        sys.exit(3)
-    except json.JSONDecodeError:
-        print(f'Erro: invalid json config file {CONFIG_PATH.absolute()}.')
-        sys.exit(3)
 
 
 if __name__ == '__main__':
